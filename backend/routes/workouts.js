865d257db1e1f2bfff832cf5071e5a26e32e257a
abi-shek-dev <abishekinterspace@gmail.com>
2026-06-30 21:58:04 +0530

    router initialization

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,5 +2,13 @@ const express = require('express')
 
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    res.json({ mssg: "Get all workouts" })
+})
+
+router.get('/:id', (req, res) => {
+    res.json({ mssg: "Get a single workout" })
+})
+
 
 module.exports = router;
