445cb2f2b57eb03fd6851ae2702c50f7fb99ae5d
abi-shek-dev <abishekinterspace@gmail.com>
2026-06-30 21:49:20 +0530

    workout router initialization

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 
+const workoutRoutes = require('./routes/workouts')
+
 
 // express app
 const app = express()
@@ -13,10 +15,7 @@ app.use((req, res, next) =>{
 
 
 // routes
-app.get('/', (req, res) =>{
-    res.json({"mssg": "Welcome to the app"})
-})
-
+app.use('/api/workouts', workoutRoutes )
 
 // listen for requests
 app.listen(process.env.PORT, ()=> {
