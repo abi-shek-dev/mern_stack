8dd7077a740a308471af055a4316dc985656ff52
abi-shek-dev <abishekinterspace@gmail.com>
2026-07-01 21:31:20 +0530

    dns issue

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+const dns = require('dns')
+dns.setServers(['8.8.8.8', '1.1.1.1'])
 const express = require('express')
 const mongoose = require('mongoose')
 
@@ -12,7 +14,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use((req, res, next) =>{
+app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 })
@@ -21,9 +23,13 @@ app.use((req, res, next) =>{
 // routes
 app.use('/api/workouts', workoutRoutes);
 
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Listening on port http://localhost:${process.env.PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
-
-// listen for requests
-app.listen(process.env.PORT, ()=> {
-    console.log(`Listening on port http://localhost:${process.env.PORT}`)
-})
