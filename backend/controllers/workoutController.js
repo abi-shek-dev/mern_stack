00c96ee96c9e54641c528d833d0fe43c670063c9
abi-shek-dev <abishekinterspace@gmail.com>
2026-07-02 23:39:48 +0530

    get one

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -18,6 +18,8 @@ const getWorkout = async (req, res) => {
   if (!workout) {
     return res.status(404).json({ error: "No such workout" });
   }
+
+  res.status(200).json(workout)
 };
 
 //  create a workout
