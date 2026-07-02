c94f7e321c4a67a0f581d90931b6c9a241a7d669
abi-shek-dev <abishekinterspace@gmail.com>
2026-07-02 22:49:23 +0530

    controller

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Workout = require("../models/workoutModel");
+const { createWorkout } = require("../controllers/workoutController");
 
 const router = express.Router();
 
@@ -11,16 +11,7 @@ router.get("/:id", (req, res) => {
   res.json({ mssg: "Get a single workout" });
 });
 
-router.post("/", async (req, res) => {
-  const { title, load, reps } = req.body;
-
-  try {
-    const workout = await Workout.create({ title, load, reps });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createWorkout )
 
 router.delete("/", (req, res) => {
   res.json({ mssg: "Delete a single workout" });
