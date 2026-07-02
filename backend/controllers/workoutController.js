740784fdc398161c66daae4eb1b102420569e10e
abi-shek-dev <abishekinterspace@gmail.com>
2026-07-02 23:36:56 +0530

    get all and get one

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -2,7 +2,23 @@ const Workout = require("../models/workoutModel");
 
 // get all workout
 
+const getWorkouts = async (req, res) => {
+  const workouts = (await Workout.find({})).sort({ createdAt: -1 });
+
+  res.status(200).json(workouts);
+};
+
 //  get a single workout
+
+const getWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  const workout = await Workout.findById(id);
+
+  if (!workout) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+};
 
 //  create a workout
 
@@ -17,12 +33,10 @@ const createWorkout = async (req, res) => {
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
-  
 };
 
 // delete a workout
 
 // update a workout
 
-
-module.exports = {createWorkout}
+module.exports = { createWorkout };
