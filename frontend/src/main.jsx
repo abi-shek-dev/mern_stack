efccbd8ccabf85d7fe8721d7e13f24abf290e2fe
abi-shek-dev <abishekinterspace@gmail.com>
2026-07-08 00:20:55 +0530

    context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import {BrowserRouter} from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
+import { WorkoutsContext } from "./context/WorkoutContext.js";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
-      <App />
+      <WorkoutsContext>
+        <App />
+      </WorkoutsContext>
     </StrictMode>
   </BrowserRouter>,
 );
