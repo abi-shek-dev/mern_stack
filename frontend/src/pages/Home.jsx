76ecea902c9d749a0cd134a6fc162e95ec12f0c1
abi-shek-dev <abishekinterspace@gmail.com>
2026-07-08 00:32:03 +0530

    home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import WorkoutDetails from "../components/WorkoutDetails";
 import WorkoutForm from "../components/WorkoutForm";
+import { useWorkoutContext } from "../hooks/useWorkouts";
 
 const Home = () => {
 
-    const [workouts, setWorkouts] = useState(null);
+  const {workouts, dispatch } = useWorkoutContext()
+
     useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await fetch("http://localhost:4000/api/workouts");
       const json = await response.json();
 
       if (response.ok) {
-        setWorkouts(json)
+        dispatch({type: 'SET_WORKOUTS', payload: json})
       }
     };
 
